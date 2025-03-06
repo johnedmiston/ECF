72c48578d7e2f1b5e8b8391ba3f0393157d3448e
Jesse Vargas <jvargas7130@gmail.com>
2025-03-05 20:58:14 -0800

    Pushing ECF-Front-end

---
+++ ecf-front-end/src/component/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+        <h3>This is the Footer </h3>
+    </div>
+  );
+}
+
+export default Footer;
